e5ef6498e4edc5978860d83a7fc5d234cba8149f
Alexandros Polymeros <alpolymer@gmail.com>
2022-10-13 19:59:23 +0300

    the switch statement

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ if (language === "English" && population < 50e6 && isIsland === false) {
     console.log("You should live in Greece!")
 }
 else { console.log("Greece does not meet your criteria...") }
+
+switch (language) {
+    case 'mandarin':
+        console.log('MOST number of native speakers!')
+        break;
+
+    case 'spanish':
+        console.log('2nd place in number of native speakers')
+        break;
+
+    case 'english':
+        console.log('3rd place')
+        break;
+
+    case 'hindi':
+        console.log('Number 4')
+        break;
+
+    case 'arabic':
+        console.log('5th most spoken language')
+        break;
+
+    default: console.log('Great language too!');
+};
